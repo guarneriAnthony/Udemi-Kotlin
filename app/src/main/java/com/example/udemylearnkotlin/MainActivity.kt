d49b1310ba66acf7e55a1e19b21b1c0d9ab4c8b2
guarneri anthony <guarneriAnthony@outlook.fr>
2023-08-23 16:38:01 +0200

    Boucle, Array
    Learn and try boucles and Array in kotlin. have see var null to.

--- app/src/main/java/com/example/udemylearnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/udemylearnkotlin/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.udemylearnkotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Arrays
 
+fun isOldEnough(age: Int): Boolean {
+    return age >= 5
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var age: Int = 10
+        val ages = arrayOf(4, 8, 20)
+        val names = Array(5){""}
+        names[0] = "Boby"
+        val array = Array(10) {0}
+        val age: Int = 10
         val name: String = "Bob"
         val height: Float = 1.60F
         val type = if (age < 18) "est un enfant" else " est un adulte"
@@ -21,8 +29,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (age > 0 && name === "Bob") {
-            println("$name" + " a " +age + " ans.")
-            println("$typeHuman")
+            println("$name a $age ans.")
+            println(typeHuman)
         }
+
+        val number : Array<String> = arrayOf("un", "deux", "trois", "quatre", "cinq")
+        for ((index, numbers) in number.withIndex()){
+            if (numbers != "quatre"){
+                println("$numbers est à lindex $index")
+                continue
+                println("8")
+            }
+
+        }
+
+        println(array.contentToString())
+        println(names.contentToString())
     }
 }
